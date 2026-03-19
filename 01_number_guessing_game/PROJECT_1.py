0cf6409c5a474b3554e080a927467422449af8d0
Adam57055 <adamtwam85@gmail.com>
2026-03-19 11:52:20 -0400

    Add comments for clarity and replace else block with elif; use continue for elif blocks

--- 01_number_guessing_game/PROJECT_1.py
+++ 01_number_guessing_game/PROJECT_1.py
@@ -1,15 +1,18 @@
+#Import
 import random
 
-x=random.randint(1,100)
+x=random.randint(1,100) #random integer selected from 1-100
 
 print("welcome to the number guessing game!")
 print("i have selected a number between 1 and 100. can you guess it?")
 while True:
-    guess=int(input("Enter a number "))
-    if guess==x:
+    guess=int(input("Enter a number ")) #converts user's input to integer
+    if guess==x: #if guess is number
         print("CONGRATULATIONS YOU GUESSED IT CORRECTLY")
-        break
-    elif guess>x:
+        break #program ends
+    elif guess>x: #if guess is higher than number
         print("too high")
-    else:
+        continue #program still runs
+    elif guess<x: #if guess is neither
         print("too low")
+        continue #program still runs
